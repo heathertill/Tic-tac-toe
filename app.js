afb48cfbc9b3957892ebc1b37b50aff6ba992d58
heathertill <heather.till@charter.net>
2018-09-10 14:49:17 -0500

    have gameboard, mark squares if empty, and alert if taken

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
-let cells = document.querySelectorAll('.cell');
+let moves = 0;
+let clicked = false;
+let squares = document.querySelectorAll('#cell');
 
-cells.forEach(function(cell) {
-    cell.addEventListener('click', cellClicked);
-})
 
-function cellClicked(e) {
 
-e.target.textContent = "hello"
+squares.forEach(function(cell) {
+    cell.addEventListener('click', mark);
+  
+});
 
+function mark(e) {
+    let icon = document.createElement('span');
+    let activeCell = e.target
+    console.log(activeCell)
+    let notEmpty = activeCell.hasAttribute('class');
+    moves++;
+ 
+    if (notEmpty == true) {
+        alert('This square is taken!')
+
+    } else if (notEmpty == false){
+        
+            if (moves % 2 == 0) {
+                e.target.appendChild(icon);
+                icon.setAttribute('class', 'glyphicon glyphicon-record');
+            } else {
+                e.target.appendChild(icon);
+                icon.setAttribute('class', 'glyphicon glyphicon-remove');
+            }
+        
+       
+    }
+
+ 
+
+
+
+    
+   
+   
+    console.log(moves);
+}
+
+function clickable(e) {
+   
+    if(e.target.innerHTML) {
+		false;
+	} 
+    
+    console.log('fired')
 }
