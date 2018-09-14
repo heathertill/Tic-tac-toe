b4c27592b29345bea030eeb056e8a868a709df15
heathertill <heather.till@charter.net>
2018-09-14 14:28:57 -0500

    working on resetting the game

--- app.js
+++ app.js
@@ -1,52 +1,93 @@
 let moves = 0;
 let clicked = false;
+let reset = 0;
 let squares = document.querySelectorAll('#cell');
+let board = document.querySelector('#gameboard');
+let players = [[], []];
+let winners = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+    ['1', '5', '9'],
+    ['3', '5', '7']
+];
 
-
+let winDiv = document.createElement('div');
+let h3Winner = document.createElement('h3');
+winDiv.appendChild(h3Winner);
+document.body.appendChild(winDiv);
+h3Winner.setAttribute('class', 'winDiv');
+let child = h3Winner.firstChild;
 
 squares.forEach(function(cell) {
     cell.addEventListener('click', mark);
-  
+    cell.addEventListener('click', isWinner);
 });
 
 function mark(e) {
     let icon = document.createElement('span');
-    let activeCell = e.target
-    console.log(activeCell)
+    let activeCell = e.target;
     let notEmpty = activeCell.hasAttribute('class');
     moves++;
- 
     if (notEmpty == true) {
-        alert('This square is taken!')
-
-    } else if (notEmpty == false){
-        
-            if (moves % 2 == 0) {
-                e.target.appendChild(icon);
-                icon.setAttribute('class', 'glyphicon glyphicon-record');
-            } else {
-                e.target.appendChild(icon);
-                icon.setAttribute('class', 'glyphicon glyphicon-remove');
-            }
-        
-       
+        alert('This square is taken!');
+    } else if (notEmpty == false) {
+        if (moves % 2 == 0 && reset == 0) {
+            e.target.appendChild(icon);
+            icon.setAttribute('class', 'glyphicon glyphicon-record');
+            let score2 = activeCell.textContent;
+            players[1].push(score2);
+        } else if (reset == 0) {
+            e.target.appendChild(icon);
+            icon.setAttribute('class', 'glyphicon glyphicon-remove');
+            let score1 = activeCell.textContent;
+            players[0].push(score1);
+        }
     }
-
- 
-
-
-
-    
-   
-   
-    console.log(moves);
+    console.log(reset);
 }
 
-function clickable(e) {
-   
-    if(e.target.innerHTML) {
-		false;
-	} 
-    
-    console.log('fired')
+function isWinner() {
+    for (i = 0; i < winners.length; i++) {
+        let checker = value =>
+            !winners[i].some(element => value.includes(element));
+        for (j = 0; j < players.length; j++) {
+            let winner = players[j].filter(checker);
+            if (winner.length == players[j].length - 3) {
+                if (moves % 2 != 0) {
+                    ++reset;
+                    console.log(reset);
+                    let h3Text = document.createTextNode(
+                        'Congratulations Player 1!!!'
+                    );
+                    h3Winner.appendChild(h3Text);
+
+                    if ((reset = 1)) {
+                        return;
+                    }
+                } else if (moves % 2 == 0) {
+                    let h3Text = document.createTextNode(
+                        'Congratulations Player 2!!!'
+                    );
+                    h3Winner.appendChild(h3Text);
+                    ++reset;
+                    console.log(reset);
+                    if ((reset = 1)) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    console.log(reset);
+}
+
+function replay() {
+    if ((reset = 1)) {
+        window.location.reload(false);
+        console.log('fire');
+    }
 }
--- scratch.js
+++ scratch.js
@@ -5,5 +5,33 @@ function clearGrid(){
 }; 
 
 
-  
+if (moves >= 5) {
+    if (player1.includes())
+}
 
+function isWinner() {
+    for (var i = 0; i < winners.length; i++) {
+        let checker = value =>
+            !winners[i].some(element => value.includes(element));
+        let winner = player1.filter(checker);
+
+        if (winner.length == player1.length - 3) {
+            
+            if (moves % 2 == 0) {
+                alert('player2 is the winner')
+                break;
+            } if (moves % 2 != 0){
+                alert('player1 is the winner')
+                break;
+            }
+         
+        }
+
+        console.log(winner.length, 'winner length');
+        console.log(player1.length, 'player1 length');
+        console.log(player1, 'player1');
+        console.log(winners[i], 'winners[i]');
+        console.log(winner, 'winner');
+        console.log('####');
+    }
+}
